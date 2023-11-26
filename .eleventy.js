61c8e0069290d3072b55196020e010d8435ca7f6
Sharona Young <msrona806@gmail.com>
2023-11-25 20:04:50 -0500

    updated config

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/reset.css');
   eleventyConfig.addPassthroughCopy('src/style.css');
   eleventyConfig.addPassthroughCopy('src/assets');
+  eleventyConfig.addPassthroughCopy('src/admin');
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
